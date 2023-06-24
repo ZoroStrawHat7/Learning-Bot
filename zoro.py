063a91d1bb04a0270f0604ffbbad6c32cf425117
ᴢᴏʀᴏ • ꜱᴛʀᴀᴡ ʜᴀᴛ <134164582+ZoroStrawHat7@users.noreply.github.com>
2023-06-24 22:13:56 +0530

    Update and rename luffu.py to zoro.py

--- zoro.py
+++ zoro.py
@@ -1,16 +1,19 @@
 from pyrogram import Client, filters
 
 # Create an instance of the bot
-bot = Client("your_bot_token")
+api_id = API_ID
+api_hash = 'API_HASH'
+bot_token = 'BOT_TOKEN'
+app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
 # Define the /start command handler
-@bot.on_message(filters.command("start"))
+@app.on_message(filters.command("start"))
 def start_command(client, message):
     # Reply to the user
     client.send_message(
         chat_id=message.chat.id,
-        text="Welcome to the bot! How can I assist you?"
+        text="Welcome to the bot!"
     )
 
 # Run the bot
-bot.run()
+app.run()
